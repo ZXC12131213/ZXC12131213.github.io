3cadb2d9b1ef960f6be9485180b2300a4ac795d3
张旭超 <zhangxuchao@shouqianba.com>
2023-04-28 18:21:12 +0800

    1

--- src/pages/Three/index.tsx
+++ src/pages/Three/index.tsx
@@ -2,7 +2,6 @@ import './style.scss';
 const ThreePage: React.FC = () => {
   return (
     <div className="home-wrap">
-      1
       <div className="content">
         <div className="content-title">打怪升级小驿站</div>
         <div className="content-user">@超早睡超晚起</div>
