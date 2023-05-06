6bb47b9609ac1644e08713e96d00f0ed780c5e09
张旭超 <zhangxuchao@shouqianba.com>
2023-05-06 22:49:22 +0800

    hash2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="home-wrap">
       <div className="content">
-        <div className="content-title">打怪升级小驿站</div>
+        <div className="content-title">打怪升级小驿站1</div>
         <div className="content-user">@超早睡超晚起</div>
       </div>
     </div>
